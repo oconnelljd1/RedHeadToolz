79ba33b35a69ab7e1879c6ba45074deb0a1fc12f
James <oconnelljd1@gmail.com>
2025-02-25 22:15:18 -0400

    Created singleton class and implemented it into audiocontroller

--- Samples/Initialization/InitController.cs
+++ Samples/Initialization/InitController.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using RedHeadToolz.Audio;
 
-namespace RedHeadToolz.Utils
+namespace RedHeadToolz.Tools
 {
     public class InitController : MonoBehaviour
     {
