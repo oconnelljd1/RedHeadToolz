dd6216c3eae80fa2170f5fd174f475678c70cbd1
James <oconnelljd1@gmail.com>
2025-03-21 00:09:03 -0300

    Version management tools, and build pipeline tool that sorts builds by platform and version

---
+++ Editor/BuildApp.cs
@@ -0,0 +1,100 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+using System.Linq;
+using RedHeadToolz.Debugging;
+
+public static class BuildApp
+{
+    [MenuItem("RedHeadToolz/Build/Build Application")]
+    public static void BuildApplication()
+    {
+        // Find project root, then go back a step
+        string projectPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(Application.dataPath, ".."));
+        string buildFolder = System.IO.Path.Combine(projectPath, "Builds");
+        
+        // Create the build folder if it doesn't exist
+        if (!System.IO.Directory.Exists(buildFolder))
+        {
+            System.IO.Directory.CreateDirectory(buildFolder);
+        }
+
+        var targetPlatform = EditorUserBuildSettings.activeBuildTarget;
+        string platformFolder = System.IO.Path.Combine(buildFolder, targetPlatform.ToString());
+
+        // Create the platform folder if it doesn't exist
+        if (!System.IO.Directory.Exists(platformFolder))
+        {
+            System.IO.Directory.CreateDirectory(platformFolder);
+        }
+
+        string projectName = Application.productName;
+        string version = PlayerSettings.bundleVersion.Replace('.', '_');
+
+        string versionFolder = System.IO.Path.Combine(platformFolder, version);
+        // Create the version folder if it doesn't exist (mainly for webGL)
+        if (!System.IO.Directory.Exists(versionFolder))
+        {
+            System.IO.Directory.CreateDirectory(versionFolder);
+        }
+
+        // Define the build path
+        string buildExtension = GetBuildExtension(targetPlatform);
+        string buildPath = System.IO.Path.Combine(versionFolder, $"{projectName}_{version}{buildExtension}");
+
+        RHTebug.Log(buildPath);
+        // return;
+        
+        string[] scenes = GetScenes();
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            locationPathName = buildPath,
+            target = EditorUserBuildSettings.activeBuildTarget,
+            options = BuildOptions.None
+        };
+
+        // Build the player
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+        // Check for build success
+        if (report.summary.result == BuildResult.Succeeded)
+        {
+            RHTebug.LogSuccess("Build succeeded: " + report.summary.totalSize + " bytes");
+        }
+        else if (report.summary.result == BuildResult.Failed)
+        {
+            RHTebug.LogError("Build failed");
+        }
+    }
+
+    private static string[] GetScenes()
+    {
+        // Get all enabled scenes in the build settings
+        return EditorBuildSettings.scenes
+            .Where(scene => scene.enabled)
+            .Select(scene => scene.path)
+            .ToArray();
+    }
+
+    private static string GetBuildExtension(BuildTarget targetPlatform)
+    {
+        switch (targetPlatform)
+        {
+            case BuildTarget.Android:
+                // add additional code for development vs release (apk vs adb)
+                return ".apk";
+            case BuildTarget.StandaloneWindows:
+            case BuildTarget.StandaloneWindows64:
+                return ".exe";
+            case BuildTarget.StandaloneOSX:
+                return ".app";
+            case BuildTarget.WebGL:
+                return "";
+            // Add other platforms as needed
+            default:
+                return "";
+        }
+    }
+}
---
+++ Editor/IncrementVersion.cs
@@ -0,0 +1,51 @@
+using UnityEditor;
+using UnityEngine;
+using RedHeadToolz.Debugging;
+using System.Linq;
+using System;
+
+public static class IncrementVersion
+{
+    [MenuItem("RedHeadToolz/Version/Increment Major")]
+    public static void IncrementMajor()
+    {
+        VerifyVersion();
+        string[] versionParts = PlayerSettings.bundleVersion.Split('.');
+        int major = int.Parse(versionParts[0]);
+        major++;
+        PlayerSettings.bundleVersion = $"{major}.0.0";
+        RHTebug.Log($"Version updated to: {PlayerSettings.bundleVersion}");
+    }
+
+    [MenuItem("RedHeadToolz/Version/Increment Minor")]
+    public static void IncrementMinor()
+    {
+        VerifyVersion();
+        string[] versionParts = PlayerSettings.bundleVersion.Split('.');
+        int major = int.Parse(versionParts[0]);
+        int minor = int.Parse(versionParts[1]);
+        minor++;
+        PlayerSettings.bundleVersion = $"{major}.{minor}.0";
+        RHTebug.Log($"Version updated to: {PlayerSettings.bundleVersion}");
+    }
+
+    [MenuItem("RedHeadToolz/Version/Increment Patch")]
+    public static void IncrementPatch()
+    {
+        VerifyVersion();
+        string[] versionParts = PlayerSettings.bundleVersion.Split('.');
+        int major = int.Parse(versionParts[0]);
+        int minor = int.Parse(versionParts[1]);
+        int patch = int.Parse(versionParts[2]);
+        patch++;
+        PlayerSettings.bundleVersion = $"{major}.{minor}.{patch}";
+        RHTebug.Log($"Version updated to: {PlayerSettings.bundleVersion}");
+    }
+
+    public static void VerifyVersion()
+    {
+        if(PlayerSettings.bundleVersion.Split('.').Length == 3) return;
+
+        PlayerSettings.bundleVersion = $"0.0.0";
+    }
+}
