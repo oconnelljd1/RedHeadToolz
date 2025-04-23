1245c22495a6c78e3e8c02f4f2076272c2cc94c0
James <oconnelljd1@gmail.com>
2025-04-23 17:24:23 -0300

    translate scren anmespace, and email validator class

--- Runtime/Screens/TranslateScreen.cs
+++ Runtime/Screens/TranslateScreen.cs
@@ -1,54 +1,56 @@
 using System;
 using DG.Tweening;
 using RedHeadToolz.Debugging;
-using RedHeadToolz.Screens;
 using UnityEngine;
 
-public class TranslateScreen : BaseScreen
+namespace RedHeadToolz.Screens
 {
-    [Header("TranslateScreen")]
-    [SerializeField] private RectTransform _root;
-    [SerializeField] private Vector2 _inStart;
-    [SerializeField] private Vector2 _outEnd;
-
-    private Vector2 rootOrigin;
-
-    void Awake()
+    public class TranslateScreen : BaseScreen
     {
-        rootOrigin = _root.anchoredPosition;
-        // RHTebug.Log($"{rootOrigin}");
-    }
+        [Header("TranslateScreen")]
+        [SerializeField] private RectTransform _root;
+        [SerializeField] private Vector2 _inStart;
+        [SerializeField] private Vector2 _outEnd;
 
-    public override void Show(Action callback = null)
-    {
-        _lockInput = true;
-        _showing = true;
+        private Vector2 rootOrigin;
 
-        _root.anchoredPosition = _inStart;
-        _root.DOAnchorPos(rootOrigin, 0.5f)
-            .OnComplete(() => {
-                callback?.Invoke();
-                _lockInput = false;
-            });
-    }
+        void Awake()
+        {
+            rootOrigin = _root.anchoredPosition;
+            // RHTebug.Log($"{rootOrigin}");
+        }
 
-    public override void ShowImmediate()
-    {
-        _root.anchoredPosition = Vector2.zero;
-        _showing = true;
-        _lockInput = false;
-    }
+        public override void Show(Action callback = null)
+        {
+            _lockInput = true;
+            _showing = true;
 
-    public override void Hide(Action callback = null)
-    {
-        _lockInput = true;
-        _showing = true;
+            _root.anchoredPosition = _inStart;
+            _root.DOAnchorPos(rootOrigin, 0.5f)
+                .OnComplete(() => {
+                    callback?.Invoke();
+                    _lockInput = false;
+                });
+        }
 
-        // _root.anchoredPosition = _inStart;
-        _root.DOAnchorPos(_outEnd, 0.5f)
-            .OnComplete(() => {
-                callback?.Invoke();
-                _lockInput = false;
-            });
+        public override void ShowImmediate()
+        {
+            _root.anchoredPosition = Vector2.zero;
+            _showing = true;
+            _lockInput = false;
+        }
+
+        public override void Hide(Action callback = null)
+        {
+            _lockInput = true;
+            _showing = true;
+
+            // _root.anchoredPosition = _inStart;
+            _root.DOAnchorPos(_outEnd, 0.5f)
+                .OnComplete(() => {
+                    callback?.Invoke();
+                    _lockInput = false;
+                });
+        }
     }
 }
